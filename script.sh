19c222390182cecf8abe98053dde8f6c0ccbf485
Saurabh Dwivedi <Devopstestuser3@outlook.com>
2022-03-24 16:34:53 +0530

    upload script.sh

---
+++ script.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+sudo apt-get update -y
+curl -sL https://deb.nodesource.com/setup_14.x | sudo bash -
+sudo apt -y install nodejs
+
+sudo mkdir myapp
+sudo cd myapp
+npm install express --save
+sudo cp /home/<username>/app.js home/<username>/myapp/   #updaten the username
+
+sudo npm install -g pm2
+sudo pm2 start app.js
+sudo pm2 status
+
+sudo apt-get install nginx -y
+sudo rm /etc/nginx/sites-available/default
+sudo cp /home/<username>/default /etc/nginx/sites-available/default  #update the username
+sudo nginx -t
+sudo service nginx restart
